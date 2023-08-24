28c01cf9cc2b64fe494dd7a403fed0a767fa79fc
Justin Davis <jdavis19@mail.wou.edu>
2023-08-24 00:56:55 -0700

    Forgot to create the branch for this project. In this commit, I'm also changing the display name of the application window to My App.

--- Demos/Simple C# Desktop Application/MyFirstProject/MyFirstProject/Form1.Designer.cs
+++ Demos/Simple C# Desktop Application/MyFirstProject/MyFirstProject/Form1.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(2304, 1026);
+            this.Name = "Form1";
+            this.Text = "My App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Demos/Simple C# Desktop Application/MyFirstProject/MyFirstProject/Form1.cs
+++ Demos/Simple C# Desktop Application/MyFirstProject/MyFirstProject/Form1.cs
@@ -6,5 +6,10 @@ namespace MyFirstProject
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
