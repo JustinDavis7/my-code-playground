f23164e03f30ed5a6cf7bada5cddbbb34dedb72e
Justin Davis <jdavis19@mail.wou.edu>
2023-08-25 00:17:54 -0700

    Created the start of the form with 4 entry text boxes that do nothing and aren't named right yet.

--- Demos/Simple C# Desktop Application/SecondProject/Econtact/Econtact/Econtact.Designer.cs
+++ Demos/Simple C# Desktop Application/SecondProject/Econtact/Econtact/Econtact.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Econtact
 {
-    partial class Form1
+    partial class Econtact
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1428, 798);
+            this.Name = "Form1";
+            this.Text = "Econtact";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Demos/Simple C# Desktop Application/SecondProject/Econtact/Econtact/Econtact.cs
+++ Demos/Simple C# Desktop Application/SecondProject/Econtact/Econtact/Econtact.cs
@@ -1,8 +1,8 @@
 namespace Econtact
 {
-    public partial class Form1 : Form
+    public partial class Econtact : Form
     {
-        public Form1()
+        public Econtact()
         {
             InitializeComponent();
         }
