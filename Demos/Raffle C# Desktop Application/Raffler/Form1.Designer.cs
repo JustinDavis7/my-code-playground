d4e4560bdb23c583a8ae010be61b77050b57aca3
Justin Davis <jdavis19@mail.wou.edu>
2023-09-15 17:31:54 -0700

    Setting up initial look of the form.
    I have a text box for name entry and am tweaking how it behaves when the window is resized. Learning more about the actions in the process.

--- Demos/Raffle C# Desktop Application/Raffler/Form1.Designer.cs
+++ Demos/Raffle C# Desktop Application/Raffler/Form1.Designer.cs
@@ -28,12 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtBoxNames = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtBoxNames
+            // 
+            this.txtBoxNames.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBoxNames.Location = new System.Drawing.Point(92, 197);
+            this.txtBoxNames.Multiline = true;
+            this.txtBoxNames.Name = "txtBoxNames";
+            this.txtBoxNames.Size = new System.Drawing.Size(771, 359);
+            this.txtBoxNames.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(92, 130);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 38);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "List of Names";
+            // 
+            // btnRun
+            // 
+            this.btnRun.BackColor = System.Drawing.Color.DarkGreen;
+            this.btnRun.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnRun.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnRun.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnRun.Location = new System.Drawing.Point(92, 593);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(251, 66);
+            this.btnRun.TabIndex = 2;
+            this.btnRun.Text = "Run Picker\r\n";
+            this.btnRun.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 15.85714F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(1531, 85);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Random Name Picker";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1531, 803);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBoxNames);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private TextBox txtBoxNames;
+        private Label label1;
+        private Button btnRun;
+        private Label label2;
     }
 }
--- Demos/Raffle C# Desktop Application/Raffler/Form1.cs
+++ Demos/Raffle C# Desktop Application/Raffler/Form1.cs
@@ -5,6 +5,18 @@ namespace Raffler
         public Form1()
         {
             InitializeComponent();
+
+            // Set the initial width of txtBoxNames to be 50% of the form's width.
+            txtBoxNames.Width = this.ClientSize.Width / 2;
+
+            // Assume textBox1 is an existing control on your form.
+            this.Resize += Form1_Resize;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            // Set the width of the text box to be 50% of the width of the form.
+            txtBoxNames.Width = this.ClientSize.Width / 2;
         }
     }
 }
