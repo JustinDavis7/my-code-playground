32996e132bb86af3f50fadb0f0b27495a584d974
Justin Davis <jdavis19@mail.wou.edu>
2023-09-15 17:37:58 -0700

    Updates to the min and max sizes of the form and text box.
    The form can now only go so small, as to avoid squishing entities into each other, as well as limiting how big a text box can get so it doesn't become obnoxiously large.

--- Demos/Raffle C# Desktop Application/Raffler/Form1.cs
+++ Demos/Raffle C# Desktop Application/Raffler/Form1.cs
@@ -6,8 +6,14 @@ namespace Raffler
         {
             InitializeComponent();
 
+            // Set the minimum size of the form.
+            this.MinimumSize = new Size(1000, 800);
+
             // Set the initial width of txtBoxNames to be 50% of the form's width.
             txtBoxNames.Width = this.ClientSize.Width / 2;
+
+            // Set the maximum width of txtBoxNames.
+            txtBoxNames.MaximumSize = new Size(1000, txtBoxNames.Height);
 
             // Assume textBox1 is an existing control on your form.
             this.Resize += Form1_Resize;
@@ -15,8 +21,8 @@ namespace Raffler
 
         private void Form1_Resize(object sender, EventArgs e)
         {
-            // Set the width of the text box to be 50% of the width of the form.
-            txtBoxNames.Width = this.ClientSize.Width / 2;
+            // Update the width of txtBoxNames to be 50% of the form's width, but not more than 500 pixels.
+            txtBoxNames.Width = Math.Min(this.ClientSize.Width / 2, txtBoxNames.MaximumSize.Width);
         }
     }
 }
