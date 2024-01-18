d5129fbeb820a9b6e59cff2de653426087f8bb27
Justin Davis <justindavis1177@gmail.com>
2024-01-17 17:01:57 -0800

    Created RelayCommand.
    This is basically a pass through from what I understand. In the view, a user can execute something and this class just allows it to pass through to another file. Not sure I fully understand why we do this, but hopefully I will later.

---
+++ Demos/Simple C# WPF Application/DataBindingAndCommands/Commands/RelayCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DataBindingAndCommands.Commands
+{
+    public class RelayCommand : ICommand //This implements the interface ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        private Action<object> _Execute { get; set; } // Method that returns void, it performs an action with the object.
+        private Predicate<object> _CanExecute { get; set; } // Method that returns a boolean, and takes in an object as an argument.
+
+        public RelayCommand(Action<object> executeMethod, Predicate<object> canExecuteMethod)
+        {
+            _Execute = executeMethod;
+            _CanExecute = canExecuteMethod;
+        }
+
+        // When a command is evoked, it will run these in order.
+        public bool CanExecute(object? parameter)
+        {
+            return _CanExecute(parameter);
+        }
+
+        public void Execute(object? parameter)
+        {
+            _Execute(parameter);
+        }
+    }
+}
