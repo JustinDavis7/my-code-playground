6f983669e3d9700a071a8493f9ad6ce4fe831836
Justin Davis <justindavis1177@gmail.com>
2024-01-17 16:43:13 -0800

    Created the User Class and scaffolded it a little.

---
+++ Demos/Simple C# WPF Application/DataBindingAndCommands/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBindingAndCommands.Models
+{
+    public class User
+    {
+        public User()
+        {
+            
+        }
+
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+
+    }
+}
