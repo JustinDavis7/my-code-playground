697be049a1e123a57eed6db1f12b0be2735d63c4
Justin Davis <justindavis1177@gmail.com>
2024-01-17 17:10:16 -0800

    MainViewModel created and fleshed out.

---
+++ Demos/Simple C# WPF Application/DataBindingAndCommands/ViewModel/MainViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using DataBindingAndCommands.Commands;
+using DataBindingAndCommands.Models;
+using Record_Book_MVVM.Views;
+
+namespace DataBindingAndCommands.ViewModel
+{
+    public class MainViewModel
+    {
+        public ObservableCollection<User> Users { get; set; }
+
+        public ICommand ShowWindowCommand { get; set; }
+
+        public MainViewModel()
+        {
+            Users = UserManager.GetUsers();
+
+            ShowWindowCommand = new RelayCommand(ShowWindow, CanShowWindow);
+        }
+
+        private bool CanShowWindow(object obj)
+        {
+            return true;
+        }
+
+        private void ShowWindow(object obj)
+        {
+            AddUser addUserWin = new AddUser();
+            addUserWin.Show();
+        }
+    }
+}
