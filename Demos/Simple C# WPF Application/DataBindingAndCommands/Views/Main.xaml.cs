943a3d5f2291acf8343445052264c7c6f1024a80
Justin Davis <justindavis1177@gmail.com>
2024-01-17 18:08:53 -0800

    Added ability to filter the list of contacts by name.
    Not sure what caused it, but the last commit made the program run properly.

--- Demos/Simple C# WPF Application/DataBindingAndCommands/Views/Main.xaml.cs
+++ Demos/Simple C# WPF Application/DataBindingAndCommands/Views/Main.xaml.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using DataBindingAndCommands.Models;
 using DataBindingAndCommands.ViewModel;
 
 namespace DataBindingAndCommands.Views
@@ -15,6 +17,17 @@ namespace DataBindingAndCommands.Views
             InitializeComponent();
             MainViewModel mainViewModel = new MainViewModel(); // We now have the instance, next we need to bind it.
             this.DataContext = mainViewModel; // Sets default binding to the VM.
+        }
+
+        private void FilterTextBox_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            UserList.Items.Filter = FilterMethod;
+        }
+
+        private bool FilterMethod(object obj)
+        {
+            var user = (User)obj;
+            return user.Name.Contains(FilterTextBox.Text, StringComparison.OrdinalIgnoreCase);
         }
     }
 }
