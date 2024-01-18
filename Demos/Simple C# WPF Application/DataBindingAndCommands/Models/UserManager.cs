92b0b85c1685039046382691c565811585403d48
Justin Davis <justindavis1177@gmail.com>
2024-01-17 16:48:05 -0800

    Can now add users, and get the list of users.

--- Demos/Simple C# WPF Application/DataBindingAndCommands/Models/UserManager.cs
+++ Demos/Simple C# WPF Application/DataBindingAndCommands/Models/UserManager.cs
@@ -9,12 +9,24 @@ namespace DataBindingAndCommands.Models
 {
     public class UserManager
     {
-        public static ObservableCollection<User> _DataBaseUser = new ObservableCollection<User>()
+        public static ObservableCollection<User> _DataBaseUsers = new ObservableCollection<User>()
         {
-            new User() { Email = "quam@aol.couk", Name = "Erich Fry" }, new User { Email = "in@yahoo.com", Name = "Virginia Salas" }, new User { Email = "luctus@google.com", Name = "Alvin Velasquez" }, new User { Email = "etiam.bibendum@hotmail.edu", Name = "Zia Hutchinson" }, new User { Email = "augue.porttitor@protonmail.org", Name = "Justin Burch" }
+            new User() { Email = "quam@aol.couk", Name = "Erich Fry" }, 
+            new User { Email = "in@yahoo.com", Name = "Virginia Salas" }, 
+            new User { Email = "luctus@google.com", Name = "Alvin Velasquez" }, 
+            new User { Email = "etiam.bibendum@hotmail.edu", Name = "Zia Hutchinson" }, 
+            new User { Email = "augue.porttitor@protonmail.org", Name = "Justin Burch" }
         };
 
+        public static ObservableCollection<User> GetUsers()
+        {
+            return _DataBaseUsers;
+        }
 
+        public static void AddUser(User user)
+        {
+            _DataBaseUsers.Add(user);
+        }
     }
 
 }
