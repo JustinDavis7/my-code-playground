75370f9760cebc6e76d223a3c0d2e6d4ec2b3af9
Justin Davis <justindavis1177@gmail.com>
2024-01-17 18:02:37 -0800

    Bound adduser data and created the .cs for it.

--- Demos/Simple C# WPF Application/DataBindingAndCommands/Views/AddUser.xaml.cs
+++ Demos/Simple C# WPF Application/DataBindingAndCommands/Views/AddUser.xaml.cs
@@ -3,8 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using DataBindingAndCommands.ViewModel;
 
 namespace DataBindingAndCommands.Views
 {
-
+    public partial class AddUser : Window
+    {
+        public AddUser()
+        {
+            InitializeComponent();
+            AddUserViewModel addUserViewModel = new AddUserViewModel();
+            this.DataContext = addUserViewModel;
+        }
+    }
 }
